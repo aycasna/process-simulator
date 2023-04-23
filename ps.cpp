14d399fb039a266031aa33c22bbbed3503a629ed
ayca <aycasena168@gmail.com>
2023-04-23 16:57:49 +0300

    comments added

--- ps.cpp
+++ ps.cpp
@@ -3,23 +3,23 @@
 
 using namespace std;
 
-
 int main(){
 
-    int SimulationCompletionTime, ProcessCompletionTime;
+    int SimulationCompletionTime, ProcessCompletionTime, ProcessType, ProcessCount;
     srand (time(NULL));
-    SimulationCompletionTime = rand() % 200 + 1;
+    SimulationCompletionTime = rand() % 200 + 1; //simülasyonun tamamlanma süresi 1 ve 200 arasında rastgele seçilir.
+    ProcessCount = rand() % 20 + 1; //toplam süreç sayısı 1 ve 20 arasında rastgele seçilir.
 
-    cout << "Simulation Completion Time: " << SimulationCompletionTime << endl;
-    
 
+    cout << "Simulation Completion Time: " << SimulationCompletionTime << endl; 
+    cout << "Process Count: " << ProcessCount << endl << endl;
     
     
-    for( int i=0; i<=SimulationCompletionTime; i = i+ProcessCompletionTime ){
-        ProcessCompletionTime = rand() % 20 + 1;
-        cout << "Process completion time: " <<ProcessCompletionTime << endl;
-        cout << "---------------------------Zaman: " << i << " saniye: olay oldu" << endl;
-        
-        
+    //döngü, simülasyon tamamlanma süresine ulaşana kadar, süreçlerin tamamlanma süreleri kadar artarak devam eder.
+    for(int i = 0; i <= SimulationCompletionTime; i = i + ProcessCompletionTime ){ 
+        ProcessCompletionTime = rand() % 20 + 1; //süreç tamamlanma süresi 1 ve 20 arasında rastgele seçilir.
+        ProcessType = rand() % ProcessCount + 1; //süreç tipi 1 ve toplam süreç sayısı arasında rastgele seçilir.
+        cout << "Process completion time: " << ProcessCompletionTime << endl;
+        cout << "Process Type: " << ProcessType << " | Time " << i << "s: event happened." << endl;      
     }
 }
