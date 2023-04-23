7ddbffd53f40add8423a7166e9948bbc9b58ad42
ayca <aycasena168@gmail.com>
2023-04-23 11:58:49 +0300

    ps.cpp updated

--- ps.cpp
+++ ps.cpp
@@ -1,3 +1,25 @@
-#include <stdio.h>     
-#include <stdlib.h>     
+#include <iostream>
 #include <time.h> 
+
+using namespace std;
+
+
+int main(){
+
+    int CompletionTime, processCount;
+    srand (time(NULL));
+    CompletionTime = rand() % 200 + 1;
+    processCount = rand() % 10 + 2;
+
+    cout << CompletionTime << endl;
+    cout << processCount << endl;
+
+    int count = 0;
+    for(int i=0; i<=CompletionTime; i++){
+        cout << "Zaman: " << i << " saniye: olay oldu" << endl;
+        count++;
+        if (count == processCount){
+            break;
+        }
+    }
+}
